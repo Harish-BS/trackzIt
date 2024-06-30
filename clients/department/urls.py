b40b61e0497b582b791f65176f0a5dd8ed639903
Harish <bsharish152@gmail.com>
2024-06-30 14:12:58 +0530

    department crud

--- clients/department/urls.py
+++ clients/department/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import DepartmentViewSet
+from clients.department import views
 
 router = DefaultRouter()
 router.register(r'departments', DepartmentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('clients/',views.department_list),
+    path('clients/<int:pk>',views.department_detail),
 ]
