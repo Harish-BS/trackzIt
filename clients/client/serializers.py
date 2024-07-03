ac9434437b5835cdafc23a3331d0b47a0761b5a4
Harish <bsharish152@gmail.com>
2024-07-03 20:19:16 +0530

    clients client crud added

--- clients/client/serializers.py
+++ clients/client/serializers.py
@@ -4,4 +4,4 @@ from .models import Client
 class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
-        fields = ['id','client_name','created_at','updated_at']
+        fields = '__all__'
